58a1920f11c39b31fc23521678e714e10a48b5c4
ACT-STUDENT <ACT-STUDENT@LAB3-PC27>
2025-08-27 14:30:03 +0800

    EncapUserInput

---
+++ Loon_EncapUserInput/Loon_EncapUserInput/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loon_EncapUserInput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        Main:
+            //ask user to input details
+            Console.Write("Enter the brand of shirt you want: ");
+            string brandName = Console.ReadLine();
+
+            Console.Write("Enter you shirt size: ");
+            string shirtSize = Console.ReadLine();
+
+            //Instantiate hte class
+            Shirt shirt = new Shirt();
+
+            //Input value 
+            shirt.BrandName = brandName;
+            shirt.ShirtSize = shirtSize;
+
+            //call the method
+            shirt.DisplayShirtInfo();
+
+            //ask user if they want another shrit
+            Console.Write("\nYou want to add another shirt? (y/n): ");
+            char choice = Convert.ToChar(Console.ReadLine());
+
+            if (choice == 'y')
+            {
+                goto Main;
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+}
---
+++ Loon_EncapUserInput/Loon_EncapUserInput/Shirt.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loon_EncapUserInput
+{
+    internal class Shirt
+    {
+        //declare fields
+        public string BrandName { get; set; }
+        public string ShirtSize { get; set; }
+
+        //mathod to dispaly
+        public void DisplayShirtInfo()
+        {
+            Console.Clear();
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("Here are the shirt details you want!");
+            Console.WriteLine("Shirt Brand: " + BrandName);
+            Console.WriteLine("Shirt size: " + ShirtSize);
+        }
+    }
+}
